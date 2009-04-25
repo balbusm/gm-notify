0fba11c95b9fca8a1af09587314ffab9c7297901
Alexander Hungenberg <alexander.hungenberg@gmail.com>
2009-04-25 23:12:44 +0200

    added config interface and sound support. Almost 0.7 ready

--- gm-notify.py
+++ gm-notify.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# gm-notify.py v0.5
+# gm-notify.py v0.7
 # a simple and lightweight GMail-Notifier for ubuntu starting at 9.04 and preferable notify-osd
 #
 # Copyright (c) 2009, Alexander Hungenberg <alexander.hungenberg@gmail.com>
@@ -21,19 +21,13 @@
 #
 import pynotify, indicate, urllib2
 import gtk, gobject
+import pygst
+pygst.require("0.10")
+import gst
+import gconf
 import sys, subprocess
 import gettext
 import gmailatom, keyring
-
-########
-### Edit this to change the check interval (value counted in seconds)
-########
-
-checkinterval = 90
-
-########
-### End of config
-########
 
 _ = gettext.translation('gm-notify', fallback=True).ugettext
 
@@ -43,18 +37,41 @@ class CheckMail():
         In the end it starts the periodic check timer and a gtk main-loop'''
         
         # Initiate pynotify and Feedreader with Gnome-Keyring Credentials
-        if not pynotify.init("GMail Notifier"):
+        if not pynotify.init(_("GMail Notifier")):
             sys.exit(-1)
         
         keys = keyring.Keyring("GMail", "mail.google.com", "http")
         if keys.has_credentials():
             creds = keys.get_credentials()
         else:
-            self.showNotification(_("No crendentials"), _("Please add credentials for mail.google.com (http) to the gnome keyring (via set-gmail-password.py script) and allow gm-notify accessing it."))
-            sys.exit(-1)
+            subprocess.Popen("/usr/local/bin/gm-notify-config.py", shell=True)
+            if keys.has_credentials():
+                creds = keys.get_credentials()
+            else:
+                self.showNotification(_("No Credentials"), _("You didn't complete the configuration. To try again please restart the GMail Notifier"))
+                sys.exit(-1)
         
         self.atom = gmailatom.GmailAtom(creds[0], creds[1])
         self.oldmail = []
+        
+        # Init gconf to read config values
+        self.client = gconf.client_get_default()
+        if self.client.get_string("/apps/gm-notify/checkinterval"):
+            checkinterval = int(float(self.client.get_string("/apps/gm-notify/checkinterval")))
+        else:
+            checkinterval = 90
+        
+        # init sound
+        if self.client.get_string("/apps/gm-notify/soundfile"):
+            self.player = gst.element_factory_make("playbin", "player")
+            self.player.set_property("video-sink", gst.element_factory_make("fakesink", "fakesink"))
+            self.player.set_property("uri", "file://" + self.client.get_string("/apps/gm-notify/soundfile"))
+            bus = self.player.get_bus()
+            bus.add_signal_watch()
+            bus.connect("message", self.gst_message)
+        else:
+            self.player = None
+        
         
         # Register with Indicator-Applet
         self.server = indicate.indicate_server_ref_default()
@@ -71,6 +88,13 @@ class CheckMail():
         self.checkmail()
         gobject.timeout_add_seconds(checkinterval, self.checkmail)
         gtk.main()
+    
+    def gst_message(self, bus, message):
+        if message.type == gst.MESSAGE_EOS:
+            self.player.set_state(gst.STATE_NULL)
+        elif message.type == gst.MESSAGE_ERROR:
+            self.player.set_state(gst.STATE_NULL)
+            print "Error: %s - %s" % message.parse_error()
     
     def serverClick(self, server):
         '''called when the server is clicked in the indicator-applet to open the gmail account'''
@@ -127,7 +151,11 @@ class CheckMail():
             else:
                 titles += "\n- " + title
         
-        # create notifications
+        # play a sound?
+        if len(newmail) > 0 and self.player:
+            self.player.set_state(gst.STATE_PLAYING)
+        
+        # create notifications and play sound
         if not "\n" in titles and not titles == "":
             self.showNotification(_("Incoming Message"), titles)
         elif "\n" in titles:
