b90c28730088602cf5a9dde40c425002e089a674
Alexander Hungenberg <alexander.hungenberg@gmail.com>
2009-04-25 23:12:44 +0200

    added config interface and sound support. Almost 0.7 ready

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@
 from distutils.core import setup
 
 setup(  name='gm-notify',
-        version='0.5',
+        version='0.7',
         description='Highly Ubuntu 9.04 integrated GMail Notifier',
         author='Alexander Hungenberg',
         author_email='alexander.hungenberg@gmail.com',
         py_modules=['gmailatom', 'keyring'],
-        scripts=['gm-notify.py', 'set-gmail-password.py'],
+        scripts=['gm-notify.py', 'gm-notify-config.py'],
         data_files=[('/usr/share/applications', ['data/gm-notify.desktop']),
+                    ('/usr/share/applications', ['data/gm-notify-config.desktop']),
+                    ('/usr/share/gm-notify', ['data/checking.gif']),
                     ('/usr/share/locale/de/LC_MESSAGES', ['po/de/gm-notify.mo'])] )
