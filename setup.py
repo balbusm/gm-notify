a4fa59cad14d75a7489ba0b3fc7b4235458e2e9c
Alexander Hungenberg <alexander.hungenberg@gmail.com>
2009-04-26 11:45:38 +0200

    Added danish translation

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(  name='gm-notify',
         data_files=[('/usr/share/applications', ['data/gm-notify.desktop']),
                     ('/usr/share/applications', ['data/gm-notify-config.desktop']),
                     ('/usr/share/gm-notify', ['data/checking.gif']),
+                    ('/usr/share/locale/da/LC_MESSAGES', ['po/da/gm-notify.mo']),
                     ('/usr/share/locale/de/LC_MESSAGES', ['po/de/gm-notify.mo']),
                     ('/usr/share/locale/ca/LC_MESSAGES', ['po/ca/gm-notify.mo'])] )
 
