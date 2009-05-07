d9911c7b67bcb3a1f321aea8f182c6d6e07f4f5d
Ken VanDine <ken.vandine@canonical.com>
2009-05-07 02:16:40 -0400

    Install the gconf schemas

--- setup.py
+++ setup.py
@@ -8,16 +8,13 @@ setup(  name='gm-notify',
         description='Highly Ubuntu 9.04 integrated GMail Notifier',
         author='Alexander Hungenberg',
         author_email='alexander.hungenberg@gmail.com',
-        py_modules=['gmailatom', 'keyring', 'gmxdgsoundlib'],
+        py_modules=['gmailatom', 'gmimap', 'keyring', 'gmxdgsoundlib'],
         scripts=['gm-notify.py', 'gm-notify-config.py'],
         data_files=[('/usr/share/applications', ['data/gm-notify.desktop']),
                     ('/usr/share/applications', ['data/gm-notify-config.desktop']),
                     ('/usr/share/gm-notify', ['data/checking.gif']),
                     ('/usr/share/gm-notify', ['gm-config.glade']),
+                    ('/usr/share/gconf/schemas', ['data/gm-notify.schema']),
                     ('/usr/share/locale/da/LC_MESSAGES', ['po/da/gm-notify.mo']),
                     ('/usr/share/locale/de/LC_MESSAGES', ['po/de/gm-notify.mo']),
                     ('/usr/share/locale/ca/LC_MESSAGES', ['po/ca/gm-notify.mo'])] )
-
-print "Installing gconf schema file"
-f = open("/dev/null", "w") # redirect stdout to /dev/null
-subprocess.Popen("gconftool --install-schema-file=data/gm-notify.schema", shell=True, stdout=f)
