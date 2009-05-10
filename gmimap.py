ba4caba5a515c5b4909a54c781906ac9cedc1c38
Alexander Hungenberg <alexander.hungenberg@gmail.com>
2009-05-10 15:09:27 +0200

    Increased version number and fixed README and setup.py

--- gmimap.py
+++ gmimap.py
@@ -1,7 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# gmimap.py v0.9
+# simple, twisted based, IMAP4 Client class to access Gmail
+#
+# Copyright (c) 2009, Alexander Hungenberg <alexander.hungenberg@gmail.com>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 from twisted.internet import reactor, protocol, ssl, defer
 from twisted.internet.protocol import ClientCreator
 from twisted.mail.imap4 import MessageSet, IMAP4Client
-import sys
 
 class IMAPClient(IMAP4Client):
     def login(self):
